2f93e6abd957b20eaf2b8a0e9f396ff6c05b7e99
Premanshu Bhagat <premtsapj@gmail.com>
2026-05-14 22:03:48 +0530

    creating objects that inherit from baseobject

---
+++ Python/streamlit-fastapi/backend/api.py
@@ -0,0 +1,12 @@
+#type: ignore
+#pylint: skip-file
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class RocketObject(BaseModel):
+    id : int
+    name : str
+    score : float
