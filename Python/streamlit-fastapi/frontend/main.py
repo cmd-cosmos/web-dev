e563028445322eaa5f8666311c07f039fa2e5c77
Premanshu Bhagat <premtsapj@gmail.com>
2026-05-14 22:12:40 +0530

    basic streamlit elements

---
+++ Python/streamlit-fastapi/frontend/main.py
@@ -0,0 +1,10 @@
+#pylint: skip-file
+#type: ignore
+
+import streamlit as st
+
+st.title("Title")
+st.header("Header")
+st.subheader("Subheader")
+
+st.text_input(label="Input Box", placeholder="input box")
