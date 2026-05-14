401dfd3532735e4426f1519b0d8abef8324aaecc
Premanshu Bhagat <premtsapj@gmail.com>
2026-05-14 22:35:01 +0530

    added a get request to retreive html data

--- Python/streamlit-fastapi/backend/api.py
+++ Python/streamlit-fastapi/backend/api.py
@@ -2,6 +2,7 @@
 #pylint: skip-file
 
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -16,3 +17,18 @@ def root():
     return {
         "Data" : "Test Data"
     }
+
+@app.get("/rockets", response_class=HTMLResponse)
+def rockets():
+    return """
+    <html>
+        <head>
+            <title>Rockets</title>
+        </head>
+        <body>
+            <h>Rockets</h>
+            <p>Rockets are really cool, they go to space.</p>
+        </body>
+        
+    </html>
+"""
